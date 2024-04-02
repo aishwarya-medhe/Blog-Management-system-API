6b076263304272693c01991217d6bb3568b35371
aishwarya-medhe <162131910+aishwarya-medhe@users.noreply.github.com>
2024-04-02 17:40:50 +0530

    Update models.py

--- comment_manage/models.py
+++ comment_manage/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 # Create your models here.
 class Comment(models.Model):
-    content=models.TextField()
+    comment_content=models.TextField()
     publication_date=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     post=models.ForeignKey('blog_manage.BlogPost',on_delete=models.CASCADE,related_name='comments')
